d020524f90d85c90ec23ed1bd04f7d80c7a4086e
Steve Matushchak <genesiskmd@gmail.com>
2023-12-14 16:06:54 +0800

    Initial Commit

---
+++ Draft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Draft
+//
+//  Created by Steve Howard on 2023/12/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Draft/DraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraftApp.swift
+//  Draft
+//
+//  Created by Steve Howard on 2023/12/14.
+//
+
+import SwiftUI
+
+@main
+struct DraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
